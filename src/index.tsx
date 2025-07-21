5395a55b50174289f66785c39d9267a7df454ac7
Smite Chow <xiaopengyou@live.com>
2025-07-21 14:23:18 +0800

    polyfill Promise.withResolvers

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,18 @@ import { LabelView } from "./views/labels.tsx";
 import { StreamView } from "./views/stream.tsx";
 import { RecordView } from "./views/record.tsx";
 
+if (typeof Promise.withResolvers === "undefined") {
+  Promise.withResolvers = <T>() => {
+    let resolve!: (value: T | PromiseLike<T>) => void;
+    let reject!: (reason?: unknown) => void;
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
+
 render(
   () => (
     <Router root={Layout}>
